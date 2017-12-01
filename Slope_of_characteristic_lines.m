75dc4a6e1427044c056e47a0bee269efbd2e39e9
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2017-12-01 23:07:20 +0100

    Update Slope_of_characteristic_lines.m

--- Slope_of_characteristic_lines.m
+++ Slope_of_characteristic_lines.m
@@ -15,31 +15,32 @@ slope_a(NX+1)=0;
 slope_u(NX+1)=0;
 y(NX+1)=0;
 for i=1:NX+1;
-    if x(i)<-2
-        y(i)=1;
+    if x(i)<-1
+        y(i)=-1;
     end
-    if x(i)>=-2 && x(i)<1
-        y(i)=0;
+    if x(i)>=-1 && x(i)<=1
+        y(i)=1;
     end
     if x(i)>1
         y(i)=-1;
     end
-    if(mod(i,NX/20)==0)
+ %   if(mod(i,NX/20)==0)
         for j=1:length(a)
-            slope_a(i)=slope_a(i)+a(j)*y(i)^(length(u)-j);
+            slope_a(i)=slope_a(i) + a(j)*y(i) ^ (length(a)-j);
         end
         for j=1:length(u)
             slope_u(i)=slope_u(i)+u(j)*y(i)^(length(u)-j);
         end
-    end
+ %   end
 end
 figure('Name','slope of u');
 plot(x,0);
 xlabel('x');
 ylabel('time');
 grid on;
+grid minor
 for i=1:NX+1
-    if(mod(i,NX/20)==0)
+%    if(mod(i,NX/20)==0)
         u_theta=atan(slope_u(i));
         if u_theta<0
             line([x(i) x(i)-2*cos(u_theta+pi/2)],[0 2*sin(u_theta+pi/2)],'Color','g');
@@ -48,15 +49,17 @@ for i=1:NX+1
         else
             line([x(i) x(i)-2*cos(u_theta+pi/2)],[0 2*sin(u_theta+pi/2)],'Color','b');
         end
-    end
+%    end
 end
 
 figure('Name','slope of shock -> a');
 xlabel('x');
 ylabel('time');
+cla
 grid on;
+grid minor
 for i=1:NX+1
-    if(mod(i,NX/20)==0)
+%    if(mod(i,NX/20)==0)
         a_theta=atan(slope_a(i));
         if a_theta<0
             line([x(i) x(i)-2*cos(a_theta+pi/2)],[0 2*sin(a_theta+pi/2)],'Color','g');
@@ -65,7 +68,7 @@ for i=1:NX+1
         else
             line([x(i) x(i)-2*cos(a_theta+pi/2)],[0 2*sin(a_theta+pi/2)],'Color','b');
         end
-    end
+%    end
 end
 fprintf('Given Polynomial = ');
 fprintf(poly2string(u));
